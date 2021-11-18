21b59e01212d999fd74037c438c8523579c774c6
fangzheli <48412604+fangzheli@users.noreply.github.com>
2021-11-19 02:51:07 +0800

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,14 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
+
+function play(){
+	document.querySelector("#play")
+}
